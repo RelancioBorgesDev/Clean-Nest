ac48bcddf4e9d911093f6aed34a2cd5d4df1193f
RelancioBorgesDev <relancio.dev@gmail.com>
2025-02-03 15:21:41 -0300

    feat: add guards and decorator for auth

---
+++ src/controllers/create-question.controller.ts
@@ -0,0 +1,17 @@
+import { Body, Request, UseGuards } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { CurrentUser } from 'src/auth/current-user-decorator';
+import { UserPayload } from 'src/auth/jwt-strategy';
+
+@Controller('/questions')
+@UseGuards(AuthGuard('jwt'))
+export class CreateQuestionController {
+  constructor() {}
+
+  @Post()
+  async handle(@CurrentUser() user: UserPayload) {
+    console.log(user.sub);
+    return 'ok';
+  }
+}
