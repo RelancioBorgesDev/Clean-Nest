b9742e8a7176a315d981b6aaf67dd063a4e0430a
RelancioBorgesDev <relancio.dev@gmail.com>
2025-03-25 17:10:48 -0300

    refactor(tests): refactor in e2e tests, added factories

--- test/factories/make-answer-comment.ts
+++ test/factories/make-answer-comment.ts
@@ -1,11 +1,14 @@
-import { faker } from '@faker-js/faker'
+import { faker } from '@faker-js/faker';
 
-import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
 
 import {
   AnswerComment,
   AnswerCommentProps,
-} from '@/domain/forum/enterprise/entities/answer-comment'
+} from '@/domain/forum/enterprise/entities/answer-comment';
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '@/infra/database/prisma/prisma.service';
+import { PrismaAnswerCommentMapper } from '@/infra/database/prisma/mappers/prisma-answer-comment-mapper';
 
 export function makeAnswerComment(
   override: Partial<AnswerCommentProps> = {},
@@ -19,7 +22,24 @@ export function makeAnswerComment(
       ...override,
     },
     id,
-  )
+  );
 
-  return answer
+  return answer;
+}
+
+@Injectable()
+export class AnswerCommentFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaAnswerComment(
+    data: Partial<AnswerCommentProps> = {},
+  ): Promise<AnswerComment> {
+    const answerComment = makeAnswerComment(data);
+
+    await this.prisma.comment.create({
+      data: PrismaAnswerCommentMapper.toPrisma(answerComment),
+    });
+
+    return answerComment;
+  }
 }
