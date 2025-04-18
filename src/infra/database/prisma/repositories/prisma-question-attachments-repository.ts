b7d90132ee1cfb0652db7516f07dcf2ceceafcce
RelancioBorgesDev <relancio.dev@gmail.com>
2025-04-18 14:12:51 -0300

    feat(attachments): add persisting attachments in the database

--- src/infra/database/prisma/repositories/prisma-question-attachments-repository.ts
+++ src/infra/database/prisma/repositories/prisma-question-attachments-repository.ts
@@ -11,12 +11,32 @@ export class PrismaQuestionAttachmentsRepository
   implements QuestionAttachmentsRepository
 {
   constructor(private prisma: PrismaService) {}
-  
-  createMany(attachments: QuestionAttachment[]): Promise<void> {
-    throw new Error('Method not implemented.');
+
+  async createMany(attachments: QuestionAttachment[]): Promise<void> {
+    if (attachments.length === 0) {
+      return;
+    }
+
+    const data = PrismaQuestionAttachmentMapper.toPrismaUpdateMany(attachments);
+    await this.prisma.attachment.updateMany(data);
   }
-  deleteMany(attachments: QuestionAttachment[]): Promise<void> {
-    throw new Error('Method not implemented.');
+
+  async deleteMany(attachments: QuestionAttachment[]): Promise<void> {
+    if (attachments.length === 0) {
+      return;
+    }
+
+    const attachmentIds = attachments.map((attachment) => {
+      return attachment.id.toString();
+    });
+
+    await this.prisma.attachment.deleteMany({
+      where: {
+        id: {
+          in: attachmentIds,
+        },
+      },
+    });
   }
 
   async findManyByQuestionId(
